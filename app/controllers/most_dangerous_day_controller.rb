9651c491e0c7c873acb84cfc2ffbd576b4bffd47
Luke Chambers <lukenatchambers@gmail.com>
2018-04-11 10:57:09 -0600

    First user story passing

--- app/controllers/most_dangerous_day_controller.rb
+++ app/controllers/most_dangerous_day_controller.rb
@@ -1,6 +1,5 @@
 class MostDangerousDayController < ApplicationController
   def index
     @asteroids = AsteroidPresenter.new(params).run
-    binding.pry
   end
 end
--- app/presenters/asteroid_presenter.rb
+++ app/presenters/asteroid_presenter.rb
@@ -19,11 +19,33 @@ class AsteroidPresenter
   end
 
   def most_dangerous_day
-    binding.pry
+    group_dangerous_asteroids.max_by do |key, value|
+      value.count
+    end
+  end
+
+  def most_dangerous_day_format
+    "#{Time.parse(most_dangerous_day[0]).strftime('%B %d, %Y')}"
   end
 
   private
 
     attr_reader :start_date, :end_date
+
+    def dangerous_asteroids
+      all_asteroids.reduce([]) do |result, asteroid|
+        if asteroid.is_potentially_dangerous == true
+          result << asteroid
+        else
+          result
+        end
+      end
+    end
+
+    def group_dangerous_asteroids
+      dangerous_asteroids.group_by do |asteroid|
+        asteroid.close_approach_date
+      end
+    end
 
 end
