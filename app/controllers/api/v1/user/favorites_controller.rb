5ab86191529da85fa7c017cecac6ff2570ee2493
Luke Chambers <lukenatchambers@gmail.com>
2018-04-14 20:52:30 -0600

    Refactor controller to SRP

--- app/controllers/api/v1/user/favorites_controller.rb
+++ app/controllers/api/v1/user/favorites_controller.rb
@@ -1,33 +1,12 @@
 class Api::V1::User::FavoritesController < ApiBaseController
   def index
-    favorites_json = current_user.favorites.map do |favorite|
-      conn = Faraday.new("https://api.nasa.gov/neo/rest/v1/neo/#{favorite.neo_reference_id}?api_key=#{ENV['NASA_API_KEY']}")
-      conn.get
-    end
-
-    favorites_json.map! do |json|
-      JSON.parse(json.body, symbolize_names: true)
-    end
-
-    built_json = favorites_json.map do |json|
-      {
-        id: Favorite.find_by(neo_reference_id: json[:neo_reference_id]).id,
-        neo_reference_id: json[:neo_reference_id],
-        user_id: current_user.id,
-        asteroid: {
-          name: json[:name],
-          is_potentially_hazardous_asteroid: json[:is_potentially_hazardous_asteroid]
-        }
-      }
-    end
-
-    render json: built_json
+    render json: FavoritedAsteroid.new(Favorite.new, current_user).json_response_index
   end
 
   def create
     favorite = Favorite.new(neo_reference_id: params[:neo_reference_id], user: current_user)
     if favorite.save
-      render json: FavoritedAsteroid.new(favorite, current_user).json_response
+      render json: FavoritedAsteroid.new(favorite, current_user).json_response_create
     else
       render json: { message: "Invalid credentials." }
     end
--- app/services/favorited_asteroid.rb
+++ app/services/favorited_asteroid.rb
@@ -5,7 +5,7 @@ class FavoritedAsteroid
     @current_user = current_user
   end
 
-  def json_response
+  def json_response_create
     {
       id: favorite.id,
       neo_reference_id: parsed_json[:neo_reference_id],
@@ -17,20 +17,46 @@ class FavoritedAsteroid
     }
   end
 
+  def json_response_index
+    favorites_json.map do |json|
+      {
+        id: Favorite.find_by(neo_reference_id: json[:neo_reference_id]).id,
+        neo_reference_id: json[:neo_reference_id],
+        user_id: current_user.id,
+        asteroid: {
+          name: json[:name],
+          is_potentially_hazardous_asteroid: json[:is_potentially_hazardous_asteroid]
+        }
+      }
+    end
+  end
+
   private
 
-    attr_reader :favorite, :current_user
+    attr_reader :favorite, :current_user, :favorites_json
 
-    def conn
+    def favorites_json
+      current_user_favorites_json.map! do |json|
+        JSON.parse(json.body, symbolize_names: true)
+      end
+    end
+
+    def current_user_favorites_json
+      current_user.favorites.map do |favorite|
+        response(favorite)
+      end
+    end
+
+    def conn(favorite)
       Faraday.new("https://api.nasa.gov/neo/rest/v1/neo/#{favorite.neo_reference_id}?api_key=#{ENV['NASA_API_KEY']}")
     end
 
-    def response
-      conn.get
+    def response(favorite)
+      conn(favorite).get
     end
 
     def parsed_json
-      parsed_json = JSON.parse(response.body, symbolize_names: true)
+      parsed_json = JSON.parse(response(favorite).body, symbolize_names: true)
     end
 
 end
